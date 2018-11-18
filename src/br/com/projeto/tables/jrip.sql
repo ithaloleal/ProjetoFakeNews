924f5e8431f324faaaaa3935c838722a4f9dbca2
Ithalo Leal <ithaloleal4@gmail.com>
2018-11-18 01:53:40 -0200

    Correção readme

---
+++ src/br/com/projeto/tables/jrip.sql
@@ -0,0 +1,10 @@
+CREATE TABLE jrip
+(
+  opiniao numeric(10,5),
+  fato numeric(10,5),
+  fake numeric(10,5),
+  twitter_id bigint NOT NULL,
+  CONSTRAINT "Ref_jrip_to_twitter" FOREIGN KEY (twitter_id)
+      REFERENCES twitter (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
